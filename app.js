699139d5f0e8efe0cc078a146063fcf9b83eae68
Anthony11-hub <mollycruz966@gmail.com>
2023-01-05 16:40:49 +0300

    added files that i might use

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// ejs
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// expresss body parser
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+app.listen(PORT, console.log(`Server running on ${PORT}`));
